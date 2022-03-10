0aa21cbec0c2755ee5d34783b7d9d26ef048d689
linjiansi <pmon.ken.1467@gmail.com>
2022-03-10 23:49:54 +0900

    add Lens

--- Packages/Lens/Sources/Lens/Lens.swift
+++ Packages/Lens/Sources/Lens/Lens.swift
@@ -1,6 +1,67 @@
-public struct Lens {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+import Combine
+import Connector
+
+@dynamicMemberLookup
+open class Lens<Content>: ContentConnectableProtocol {
+
+    open var contents: Published<Content>.Publisher {
+        $content
+    }
+
+    @Published internal var content: Content {
+        willSet {
+            willUpdate(to: newValue, current: content)
+        }
+        didSet {
+            didUpdate(content: content)
+        }
+    }
+
+    public required convenience init?(from input: Any) {
+        if let content = input as? Content {
+            self.init(with: content)
+        }
+        return nil
+    }
+
+    public init(with content: Content) {
+        self.content = content
+        postInit()
+    }
+
+    open func postInit() {}
+
+    open func willUpdate(
+        to content: Content,
+        current: Content
+    ) {}
+
+    open func didUpdate(
+        content: Content
+    ) {}
+
+    public func getContent<Self>(for _: Self) -> Content {
+        content
+    }
+
+    open subscript<U>(
+        dynamicMember keyPath: ReferenceWritableKeyPath<Content, U>
+    ) -> U {
+        set { content[keyPath: keyPath] = newValue }
+        get { content[keyPath: keyPath] }
+    }
+
+    open subscript<U>(
+        dynamicMember keyPath: WritableKeyPath<Content, U>
+    ) -> U {
+        set { content[keyPath: keyPath] = newValue }
+        get { content[keyPath: keyPath] }
+    }
+
+    open subscript<U>(
+        dynamicMember keyPath: KeyPath<Content, U>
+    ) -> U {
+        get { content[keyPath: keyPath] }
     }
 }
--- Packages/Utilities/Sources/Connector/ContentConnectable.swift
+++ Packages/Utilities/Sources/Connector/ContentConnectable.swift
@@ -1,9 +1,12 @@
 
+import Combine
+
 public protocol AnyContentConnectable {
     init?(from input: Any)
 }
 
 public protocol ContentConnectableProtocol: AnyContentConnectable {
     associatedtype Content
+    var contents: Published<Content>.Publisher { get }
     func getContent<Self>(for _: Self) -> Content
 }
