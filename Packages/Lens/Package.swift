a6c9e3769c75fd9dec1a54382a193e5f148a2ef6
linjiansi <pmon.ken.1467@gmail.com>
2022-03-10 23:49:10 +0900

    fix Lens package configuration

--- Packages/Lens/Package.swift
+++ Packages/Lens/Package.swift
@@ -5,24 +5,32 @@ import PackageDescription
 
 let package = Package(
     name: "Lens",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Lens",
-            targets: ["Lens"]),
+            targets: ["Lens"]
+        ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            name: "Utilities",
+            path: "../Utilities"
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Lens",
-            dependencies: []),
+            dependencies: [
+                "Utilities"
+            ]
+        ),
         .testTarget(
             name: "LensTests",
-            dependencies: ["Lens"]),
+            dependencies: ["Lens"]
+        ),
     ]
 )
